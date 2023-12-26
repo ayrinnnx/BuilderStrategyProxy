b340e95e8f125085dbd6bc037f8a2b6fa180d436
ayrinnnx <kokhan.pn@ucu.edu.ua>
2023-12-26 22:27:32 +0200

    Task two changes
    Signed-off-by: ayrinnnx <kokhan.pn@ucu.edu.ua>

--- src/main/java/org/example/two/Main.java
+++ src/main/java/org/example/two/Main.java
@@ -3,9 +3,8 @@ package org.example.two;
 public class Main {
     public static void main(String[] args) {
         Client someone = new Client("Someone", 45, "m");
-        Client someoneTwo = new Client("SomeoneTwo", 37, "w");
 
-        MailInfo mailInfo = new MailInfo(someoneTwo, "spam");
+        MailInfo mailInfo = new MailInfo(someone, "spam");
         MailBox mailBox = new MailBox();
         mailBox.addMailInfo(mailInfo);
         mailBox.sendAll();
